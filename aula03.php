be83b71ac05631e19225c635a8365a9f6b85d8c3
Aluno <evertondantas81@gmail.com>
2020-03-03 09:51:31 -0300

    Primeiro

---
+++ aula03.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <style media="screen">
+  .noticias h2{
+    color: blue;
+  }
+  table, th, td {
+    border: 1px solid black;
+  }
+  </style>
+
+  <h2> Portal de Notícias IFPR </h2>
+
+  <div class = "noticias">
+    <h2> Guilherme, Pyong e Gizelly estão no sexto paredão do BBB 20 </h2>
+    <img src="https://conteudo.imguol.com.br/c/entretenimento/ad/2020/03/02/montagem---paredao-1583118270823_v2_900x506.jpg">
+    <h6>
+      Escrito em 02 de março de 2020
+      por UOL
+    </h6>
+    <p> Gizelly, Guilherme e Pyong estão no sexto paredão do BBB 20, formado na noite desse ultimo domingo.</p>
+    <p> Prior venceu a prova bate e volta e escapou da berlinda. Anjo da semana, Guilherme imunizou Flayslane. </p>
+    <p> Em seguida, o modelo foi indicado por Ivy, e escolheu Pyong para ir ao paredão junto com ele. Prior foi o mais votado pela casa, com seis votos, e escolheu Gizelly para encarar a berlinda. Gizelly, Prior e Pyong puderam disputar a prova bate e volta. A dinâmica era de sorte. "Serão quatro rodadas, vocês deverão pegar uma carta e virar, quem pegar a carta mais alta, marca um ponto", explicou o apresentador.</p>
+  </div>
+
+  <div class = "noticias">
+    <h2> Veja como converter dólar em real </h2>
+    <p> Cada moeda do mundo tem um determinado valor, o que chamamos de taxa de câmbio. Essa taxa varia de acordo com as flutuações do mercado. Ou seja, a oferta e a demanda pela moeda no mundo fazem com que ela se valorize ou perca valor.</p>
+    <p> Por padrão, a taxa de câmbio das moedas é expressa pela relação com a moeda americana. Então, quando queremos saber quanto vale o real, esse número vem sempre comparado ao dólar: hoje o dólar comercial está cotado em R$ 4,48. Veja abaixo: </p>
+    <table style="width:100%">
+      <tr>
+        <th>Dólar comercial (US$)</th>
+        <th>Real (R$)</th>
+      </tr>
+      <tr>
+        <td>US$ 1 (um dólar)</td>
+        <td>R$ 4,48</td>
+      </tr>
+      <tr>
+        <td>US$ 2 (dois dólares)</td>
+        <td>R$ 8,96</td>
+      </tr>
+      <tr>
+        <td>US$ 5 (cinco dólares)</td>
+        <td>R$ 22,40</td>
+      </tr>
+      <tr>
+        <td>US$ 10 (dez dólares)</td>
+        <td>R$ 44,80</td>
+      </tr>
+      <tr>
+        <td>US$ 25 (vinte e cinco dólares)</td>
+        <td>R$ 112,00</td>
+      </tr>
+      <tr>
+        <td>US$ 50 (cinquenta dólares)</td>
+        <td>R$ 224,00</td>
+      </tr>
+      <tr>
+        <td>US$ 100 (cem dólares)</td>
+        <td>R$ 448,00</td>
+      </tr>
+      <tr>
+        <td>US$ 500 (quinhentos dólares)</td>
+        <td>R$ 2.240,00</td>
+      </tr>
+      <tr>
+        <td>US$ 1.000 (mil dólares)</td>
+        <td>R$ 4.480,00</td>
+      </tr>
+      <tr>
+        <td>US$ 10.000 (dez mil dólares)</td>
+        <td>R$ 44.800,00</td>
+      </tr>
+    </table>
+  </div>
+
+  <div class = "noticias">
+    <h2> Ibope não muda sem Silvia e SBT prevê mais uma semana só de desenhos </h2>
+    <img src="https://conteudo.imguol.com.br/c/entretenimento/a2/2020/02/19/silvia-abravanel-e-a-equipe-do-bom-dia-e-cia-1582134136527_v2_900x506.png">
+    <h6>
+      Escrito em 29 de fevereiro de 2020
+      por Mauricio Stycer Colunista do UOL
+    </h6>
+    <p> Afastada por vontade própria do "Bom Dia & Cia", Silvia Abravanel está nos Estados Unidos e ainda não resolveu a sua situação profissional. Há oito dias, ela pediu demissão do cargo e desde então o SBT tem exibido apenas desenhos na faixa matinal, sem uma apresentadora.</p>
+    <p> A emissora, segundo a coluna apurou, recebeu instruções para continuar exibindo desenhos na próxima semana. </p>
+
+
+  </body>
+  </html>
