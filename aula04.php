7e38dfd3c22944d6b9fad492768469d1caae3aa7
Aluno <evertondantas81@gmail.com>
2020-03-10 10:01:17 -0300

    segunda

---
+++ aula04.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS  e HTML</title>
+    <!-- primeiro importe as fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ropa+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+    <link rel="stylesheet" href="aula04.css">
+  </head>
+
+  <body>
+<div class="site">
+
+  <div class="topo">
+  <h2> Topo do site </h2>
+  </div>
+
+  <div class="conteudo">
+  Corpo do site
+  </div>
+
+ <div class="rodape">
+ <h3> Desenvolvido por Everton </h3>
+ </div>
+
+</div>
+</body>
+</html>
