44ebb2d85d79cfb8b98effabb54384cf13e5e955
Aluno <evertondantas81@gmail.com>
2020-03-16 09:51:51 -0300

    terceira

---
+++ aula05.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 05 - CSS  e HTML</title>
+    <!-- primeiro importe as fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ropa+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <!-- Toda importação de CSS deve ser feita no HEAD -->
+    <link rel="stylesheet" href="aula05.css">
+  </head>
+
+  <body>
+<div class="site">
+
+  <div class="topo">
+  <h2> Criação de menu e botões </h2>
+
+    <div class="menu">
+      <ul>
+        <li><a href="#">Página inicial</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Cadastro</a></li>
+        <li><a href="#">Contato</a></li>
+        <li><a href="#">Ajuda</a></li>
+    </div>
+  </div>
+
+  <div class="conteudo">
+  <h3>Criação de Botões</h3>
+
+  <a href="#" class="botao botao-azul">Botão Azul</a>
+  <a href="#" class="botao botao-roxo">Botão Roxo</a>
+  <a href="#" class="botao botao-rosa">Botão Rosa</a>
+  </div>
+
+ <div class="rodape">
+ <h3> Desenvolvido por Everton </h3>
+ </div>
+
+</div>
+</body>
+</html>
